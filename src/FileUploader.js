bacb9b741be35299f3de742a59071d157949d425
Evan West <ewest17@uncc.edu>
2024-01-29 03:13:38 -0500

    update indicators

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -82,19 +82,21 @@ const FileUploader = ({ onMerge }) => {
             <label for="file-input" class="file-label">Choose Files</label>
             <input type="file" id="file-input" multiple onChange={handleFileChange} className="file-input" />
         </div>
+        <span className="file-display">
+            {files.length > 0 ? `${files.length} file${files.length > 1 ? 's' : ''} selected` : 'No files selected'}
+        </span>
+        <br></br>
         <div className="indicator-selector">
             <label htmlFor="indicator-select">Select Indicator:</label>
             <select id="indicator-select" onChange={(e) => setSelectedIndicator(e.target.value)}>
             <option value="">-- Select --</option>
-            <option value="Indicator 1">Indicator 1</option>
-            <option value="Indicator 2">Indicator 2</option>
-            <option value="Indicator 3">Indicator 3</option>
-            <option value="Indicator 4">Indicator 4</option>
+            <option value="маловероятно, что ввезем">маловероятно, что ввезем</option>
+            <option value="запрет на ввоз">запрет на ввоз</option>
+            <option value="МПТ Прекурсор">МПТ Прекурсор</option>
+            <option value="Яд, маловероятно">Яд, маловероятно</option>
             </select>
         </div>
-        <span className="file-display">
-            {files.length > 0 ? `${files.length} file${files.length > 1 ? 's' : ''} selected` : 'No files selected'}
-        </span>
+        <br></br>
         {loading ? (
         <div className="loading-animation">
             <div className="loading-spinner"></div>
